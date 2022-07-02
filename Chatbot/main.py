cc01d5b761f237b10ca58f019ba67a5d167323d9
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2022-07-02 14:20:32 +1000

    restructured chatbot

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,118 +1,202 @@
 from re import split
 
 
-determiners = [
-    "the",
-    "a",
-    "an",
-    "this",
-    "that",
-    "these",
-    "those",
-    "my",
-    "your",
-    "his",
-    "her",
-    "its",
-    "our",
-    "us",
-    "we",
-    "them",
-    "their",
-    "other",
-    "another",
-    "such",
-    "what",
-    "whatever",
-    "which",
-    "whichever",
-    "rather",
-    "quite",
-    "last",
-    "next",
-    "certain",
-]
+class SENTENCE(list):
+    def __repr__(self) -> str:
+        return "SENTENCE(%s)" % " ".join([repr(w) for w in self.copy()])
+class NOUN(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (n)"
+class ADJ(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (adj)"
+class ADV(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (adv)"
+class VERB(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (v)"
+class AUX(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (aux)"
+class DET(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (d)"
+class PRON(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (pn)"
+class PREP(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (pre)"
+class INT(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (int)"
+class CONJ(str):
+    def __repr__(self) -> str:
+        return super().__repr__().strip("'\"") + " (cj)"
 
-quantifiers_distributives = [
-    "few",
-    "fewer",
-    "fewest",
-    "little",
-    "less",
-    "least",
-    "much",
-    "many",
-    "more",
-    "most",
-    "lot",
-    "lots",
-    "most",
-    "some",
-    "any",
-    "enough",
-    "all",
-    "both",
-    "half",
-    "either",
-    "neither",
-    "each",
-    "every",
-    "several",
-    "few",
-]
+class Parse:
+    determiners = [
+        "the",
+        "a",
+        "an",
+        "this",
+        "that",
+        "these",
+        "those",
+        "my",
+        "your",
+        "his",
+        "her",
+        "its",
+        "our",
+        "us",
+        "we",
+        "them",
+        "their",
+        "other",
+        "another",
+        "such",
+        "what",
+        "whatever",
+        "which",
+        "whichever",
+        "rather",
+        "quite",
+        "last",
+        "next",
+        "certain",
+    ]
+    quantifiers_distributives = [
+        "few",
+        "fewer",
+        "fewest",
+        "little",
+        "less",
+        "least",
+        "much",
+        "many",
+        "more",
+        "most",
+        "lot",
+        "lots",
+        "most",
+        "some",
+        "any",
+        "enough",
+        "all",
+        "both",
+        "half",
+        "either",
+        "neither",
+        "each",
+        "every",
+        "several",
+        "few",
+    ]
+    adjective_suffixes = [
+        "able",
+        "ible",
+        "al",
+        "ful",
+        "ian",
+        "ive",
+        "less",
+        "like",
+        "ly",
+        "ous",
+        "free",
+    ]
+    verb_suffixes = [
+        "ed",
+        "en",
+        "er",
+        "es",
+        "ing",
+        "ize",
+        "ise",
+    ]
 
-adjective_suffixes = [
-    "able",
-    "ible",
-    "al",
-    "ful",
-    "ian",
-    "ive",
-    "less",
-    "like",
-    "ly",
-    "ous"
-]
+    def __init__(self, sentence: str) -> None:
+        self.sentence = [w for w in split(r" ", sentence.lower()) if w]
+        self.out = SENTENCE()
 
-verb_suffixes = [
-    "ed",
-    "en",
-    "er",
-    "ing",
-    "ize",
-    "ise",
-    "ened",
-    "ered",
-    "ized",
-    "isee",
-]
+    def __call__(self) -> list:
+        while True:
+            if self.sentence[0] in self.determiners:
+                self.out.append(DET(self.sentence.pop(0)))
 
+            elif self.sentence[0] in self.quantifiers_distributives:
+                self.out.append(DET(self.sentence.pop(0)))
+
+            elif self.sentence[1] in self.quantifiers_distributives:
+                self.out.append(DET(self.sentence.pop(0)))
+
+            elif any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
+                if any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
+                    self.out.append(ADV(self.sentence.pop(0)))
+                
+                    while any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
+                        self.out.append(AUX(self.sentence.pop(0)))
+                    
+                    self.out.append(VERB(self.sentence.pop(0)))
+                
+                else:
+                    self.out.append(ADJ(self.sentence.pop(0)))
+
+            elif self.sentence[0] == "of":
+                self.out.append(PREP(self.sentence.pop(0)))
+
+            else:
+                break
+        
+        self.out.append(NOUN(self.sentence.pop(0)))
+
+        while any(self.sentence[0].endswith(s) for s in self.adjective_suffixes) or any(self.sentence[0].endswith(s) for s in self.verb_suffixes):
+            if any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
+                self.out.append(ADV(self.sentence.pop(0)))
+
+            elif any(self.sentence[0].endswith(s) for s in self.verb_suffixes):
+                while any(self.sentence[1].endswith(s) for s in self.verb_suffixes):
+                    self.out.append(AUX(self.sentence.pop(0)))
+                
+                self.out.append(VERB(self.sentence.pop(0)))
+
+        while len(self.sentence) > 1:
+            if self.sentence[0] in self.determiners:
+                self.out.append(DET(self.sentence.pop(0)))
+
+            elif self.sentence[0] in self.quantifiers_distributives:
+                self.out.append(DET(self.sentence.pop(0)))
+
+            elif self.sentence[1] in self.quantifiers_distributives:
+                self.out.append(DET(self.sentence.pop(0)))
+
+            elif any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
+                if any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
+                    self.out.append(ADV(self.sentence.pop(0)))
+                
+                    while any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
+                        self.out.append(AUX(self.sentence.pop(0)))
+                    
+                    self.out.append(VERB(self.sentence.pop(0)))
+                
+                else:
+                    self.out.append(ADJ(self.sentence.pop(0)))
+
+            elif self.sentence[0] == "of":
+                self.out.append(PREP(self.sentence.pop(0)))
+
+            else:
+                break
+        
+        self.out.append(NOUN(self.sentence.pop(0)))
+
+        self.out += self.sentence
+    
+        return self.out
 
 while True:
-    sentence = [w for w in split(r" |('[a-z]+)", input("> ").lower()) if w]
-    noun_phrase = ""
-    verb_phrase = ""
+    p = Parse(input("> "))
 
-    while True:
-        if sentence[0] in determiners:
-            pass
-        elif sentence[0] in quantifiers_distributives:
-            pass
-        elif sentence[1] in quantifiers_distributives:
-            pass
-        elif any(sentence[0].endswith(s) for s in adjective_suffixes):
-            if any(sentence[1].endswith(s) for s in verb_suffixes):
-                noun_phrase += sentence.pop(0) + " "
-        elif sentence[0] == "of":
-            pass
-        else:
-            break
-
-        noun_phrase += sentence.pop(0) + " "
-
-    noun_phrase += sentence.pop(0)
-
-    verb_phrase = " ".join(sentence)
-
-
-    print(noun_phrase, verb_phrase, sep="\n")
+    print(p())
