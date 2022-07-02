2a48658b5bf3ee7d1134603fa7debd13e7b1de43
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2022-07-03 07:45:16 +1000

    major changes

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -116,12 +116,216 @@ class Parse:
         "ize",
         "ise",
     ]
+    prepositions = [
+        "a",
+        "aboard",
+        "about",
+        "above",
+        "abreast",
+        "abroad",
+        "absent",
+        "across",
+        "adrift",
+        "aft",
+        "after",
+        "against",
+        "ahead",
+        "aloft",
+        "along",
+        "alongside",
+        "although",
+        "amid",
+        "amidst",
+        "among",
+        "amongst",
+        "anti",
+        "apart",
+        "apropos",
+        "around",
+        "as",
+        "ashore",
+        "aside",
+        "aslant",
+        "astride",
+        "at",
+        "atop",
+        "away",
+        "back",
+        "bar",
+        "barring",
+        "because",
+        "before",
+        "beforehand",
+        "behind",
+        "below",
+        "beneath",
+        "beside",
+        "besides",
+        "between",
+        "beyond",
+        "but",
+        "by",
+        "circa",
+        "come",
+        "concerning",
+        "considering",
+        "contra",
+        "counting",
+        "despite",
+        "down",
+        "downhill",
+        "downstage",
+        "downstairs",
+        "downstream",
+        "downwind",
+        "during",
+        "east",
+        "effective",
+        "ere",
+        "except",
+        "excepting",
+        "excluding",
+        "failing",
+        "following",
+        "for",
+        "forth",
+        "from",
+        "given",
+        "granted",
+        "hence",
+        "henceforth",
+        "here",
+        "hereby",
+        "herein",
+        "hereof",
+        "hereto",
+        "herewith",
+        "home",
+        "if",
+        "in",
+        "including",
+        "indoors",
+        "inside",
+        "into",
+        "less",
+        "lest",
+        "like",
+        "mid",
+        "midst",
+        "minus",
+        "modulo",
+        "near",
+        "neath",
+        "next",
+        "north",
+        "northeast",
+        "northwest",
+        "notwithstanding",
+        "now",
+        "of",
+        "off",
+        "offshore",
+        "on",
+        "once",
+        "onto",
+        "opposite",
+        "out",
+        "outdoors",
+        "outside",
+        "over",
+        "overboard",
+        "overhead",
+        "overland",
+        "overseas",
+        "pace",
+        "past",
+        "pending",
+        "per",
+        "plus",
+        "post",
+        "pre",
+        "pro",
+        "provided",
+        "providing",
+        "qua",
+        "re",
+        "regarding",
+        "respecting",
+        "round",
+        "sans",
+        "save",
+        "saving",
+        "seeing",
+        "short",
+        "since",
+        "so",
+        "south",
+        "southeast",
+        "southwest",
+        "sub",
+        "supposing",
+        "than",
+        "then",
+        "thence",
+        "thenceforth",
+        "there",
+        "thereby",
+        "therein",
+        "thereof",
+        "thereto",
+        "therewith",
+        "though",
+        "through",
+        "throughout",
+        "till",
+        "times",
+        "to",
+        "together",
+        "toward",
+        "towards",
+        "under",
+        "underfoot",
+        "underground",
+        "underneath",
+        "unless",
+        "unlike",
+        "until",
+        "up",
+        "uphill",
+        "upon",
+        "upstage",
+        "upstairs",
+        "upstream",
+        "upwind",
+        "versus",
+        "via",
+        "vice",
+        "wanting",
+        "west",
+        "when",
+        "whence",
+        "whenever",
+        "where",
+        "whereas",
+        "whereby",
+        "wherein",
+        "whereto",
+        "wherever",
+        "wherewith",
+        "while",
+        "whilst",
+        "with",
+        "within",
+        "without"]
 
     def __init__(self, sentence: str) -> None:
         self.sentence = [w for w in split(r" ", sentence.lower()) if w]
         self.out = SENTENCE()
 
     def __call__(self) -> list:
+        while any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
+            self.out.append(ADV(self.sentence.pop(0)))
+
         while True:
             if self.sentence[0] in self.determiners:
                 self.out.append(DET(self.sentence.pop(0)))
@@ -131,6 +335,9 @@ class Parse:
 
             elif self.sentence[1] in self.quantifiers_distributives:
                 self.out.append(DET(self.sentence.pop(0)))
+
+            elif self.sentence[0] in self.prepositions or self.sentence[0].endswith("ward") or self.sentence[0].endswith("wards"):
+                self.out.append(PREP(self.sentence.pop(0)))
 
             elif any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
                 if any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
@@ -143,9 +350,6 @@ class Parse:
                 
                 else:
                     self.out.append(ADJ(self.sentence.pop(0)))
-
-            elif self.sentence[0] == "of":
-                self.out.append(PREP(self.sentence.pop(0)))
 
             else:
                 break
@@ -172,6 +376,9 @@ class Parse:
             elif self.sentence[1] in self.quantifiers_distributives:
                 self.out.append(DET(self.sentence.pop(0)))
 
+            elif self.sentence[0] in self.prepositions or self.sentence[0].endswith("ward") or self.sentence[0].endswith("wards"):
+                self.out.append(PREP(self.sentence.pop(0)))
+
             elif any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
                 if any(self.sentence[1].endswith(s) and self.sentence[1] not in (self.quantifiers_distributives + self.determiners) for s in self.verb_suffixes):
                     self.out.append(ADV(self.sentence.pop(0)))
@@ -184,15 +391,18 @@ class Parse:
                 else:
                     self.out.append(ADJ(self.sentence.pop(0)))
 
-            elif self.sentence[0] == "of":
-                self.out.append(PREP(self.sentence.pop(0)))
-
             else:
                 break
         
-        self.out.append(NOUN(self.sentence.pop(0)))
+        while self.sentence:
+            if any(self.sentence[0].endswith(s) for s in self.adjective_suffixes):
+                self.out.append(ADV(self.sentence.pop(0)))
 
-        self.out += self.sentence
+            elif self.sentence[0] in self.prepositions or self.sentence[0].endswith("ward") or self.sentence[0].endswith("wards"):
+                self.out.append(PREP(self.sentence.pop(0)))
+
+            else:
+                self.out.append(NOUN(self.sentence.pop(0)))
     
         return self.out
 
